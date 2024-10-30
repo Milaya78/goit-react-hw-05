79275b599d028294112379de42d48876bc65f0b2
Milaya78 <ela_keyt@mail.ru>
2024-10-30 20:38:17 +0100

    fix token

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const url = "https://api.themoviedb.org/3";
-const TOKEN = import.meta.env.VITE_API_KEY;
+const TOKEN =
+  "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyODUzMWQ2YTIyYjMwYmU5NTVmNTE2NDU3ODc2ZWU3MiIsIm5iZiI6MTczMDAxNzI3NC4yMjMxMjUsInN1YiI6IjY3MWRlZTJlOWZmNjgxZDllMGE0NWI4MSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.rZXN2msLyzeFamabwFjvcW5CAv-jklT7ej4Zu_9ipNU";
 
 const apiInstance = axios.create({
   baseURL: url,
